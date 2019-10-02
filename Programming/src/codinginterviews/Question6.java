6eaab07ba0a10d8ca6a8584d2d4a3e4886c3aed5
ChijinLoujue <chijinloujue@qq.com>
2019-10-02 20:01:52 +0800

    add the question6 of codingInterview

--- Programming/src/codinginterviews/Question6.java
+++ Programming/src/codinginterviews/Question6.java
@@ -2,17 +2,38 @@ package codinginterviews;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Scanner;
 
 public class Question6{
     public static void main(String []args) {
-        ListNode head = new ListNode(0);
-        System.out.println(printListFromTailToHead(i));
-    }
-    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
-        ArrayList<Integer> x = new ArrayList<Integer>();
-        while (listNode.next!=null){
-            x.add();
+        ListNode listNode = new ListNode(0);
+        ListNode temp = listNode;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i =0;i<n;++i){
+            temp.next = new ListNode(sc.nextInt());
+            temp=temp.next;
         }
+        listNode = listNode.next;
+        System.out.println(printListFromTailToHead(listNode));
+    }
+    public static ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        if (listNode==null){
+            return result;
+        }
+        temp.add(listNode.val);
+        while (listNode.next!=null){
+            listNode = listNode.next;
+            temp.add(listNode.val);
+        }
+        for (int i =temp.size()-1;i>=0;--i){
+            result.add(temp.get(i));
+        }
+        return result;
     }
 
     public static class ListNode {
