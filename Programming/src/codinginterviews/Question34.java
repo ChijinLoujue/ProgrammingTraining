9da19e15060804655aed9f10ccd81f9973221dfa
ChijinLoujue <chijinloujue@qq.com>
2019-10-15 16:35:47 +0800

    add question35 of codingInterview

--- Programming/src/codinginterviews/Question34.java
+++ Programming/src/codinginterviews/Question34.java
@@ -3,8 +3,6 @@ package codinginterviews;
 import java.util.ArrayList;
 
 public class Question34 {
-
-
     public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
         if(root==null)
             return new ArrayList<ArrayList<Integer>>();
@@ -13,27 +11,31 @@ public class Question34 {
         return FindPath(root,currentPath,currentSum,target);
     }
 
+
+
     public ArrayList<ArrayList<Integer>> FindPath(
-            TreeNode thisNode,
+            TreeNode root,
             ArrayList<Integer>currentPath,
-            int currentSum,int target) {
+            int currentSum,int x) {
 
         ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-        currentSum +=thisNode.val;
-        currentPath.add(thisNode.val);
-        if(thisNode.right==null&&thisNode.left==null){
-            if(currentSum==target){
+        currentSum +=root.val;
+        currentPath.add(root.val);
+        if(root.right==null&&root.left==null){
+            if(currentSum==x){
                 result.add(new ArrayList<Integer>(currentPath));
             }
         }else {
-            if(thisNode.left!=null){
-                result.addAll(FindPath(thisNode.left,currentPath,currentSum,target));
+            if(root.left!=null){
+                result.addAll(FindPath(root.left,currentPath,currentSum,x));
             }
-            if(thisNode.right!=null){
-                result.addAll(FindPath(thisNode.right,currentPath,currentSum,target));
+            if(root.right!=null){
+                result.addAll(FindPath(root.right,currentPath,currentSum,x));
             }
         }
         currentPath.remove(currentPath.size()-1);
         return result;
     }
 }
+
+
---
+++ Programming/src/codinginterviews/Question35.java
@@ -0,0 +1,74 @@
+package codinginterviews;
+
+import java.util.HashMap;
+
+public class Question35 {
+
+    public RandomListNode Clone(RandomListNode pHead)
+    {
+        if(pHead ==null)
+            return null;
+        RandomListNode result = new RandomListNode(pHead.label);
+        RandomListNode temp = pHead;
+        RandomListNode rtemp = result;
+        HashMap<RandomListNode,RandomListNode> nMap = new HashMap<RandomListNode,RandomListNode>();
+        nMap.put(pHead,result);
+        while (temp.next != null){
+            temp = temp.next;
+            rtemp.next = new RandomListNode(temp.label);
+            rtemp = rtemp.next;
+            nMap.put(temp,rtemp);
+        }
+        result.random = nMap.get(pHead.random);
+        temp = pHead;
+        rtemp = result;
+        while (temp.next != null){
+            temp = temp.next;
+            rtemp = rtemp.next;
+            if(temp.random !=null){
+                rtemp.random = nMap.get(temp.random);
+            }
+        }
+        return result;
+    }
+
+    public RandomListNode Clone1(RandomListNode pHead)
+    {
+        if(pHead ==null)
+            return null;
+        RandomListNode pHeadtemp = pHead;
+        while (pHeadtemp != null){
+            RandomListNode copyNode = new RandomListNode(pHeadtemp.label);
+            copyNode.next = pHeadtemp.next;
+            pHeadtemp.next = copyNode;
+            pHeadtemp = copyNode.next;
+        }
+        pHeadtemp = pHead;
+        while (pHeadtemp != null){
+            if(pHeadtemp.random !=null)
+            pHeadtemp.next.random = pHeadtemp.random.next;
+            pHeadtemp = pHeadtemp.next.next;
+        }
+        pHeadtemp = pHead;
+        RandomListNode pCopy = pHead.next;
+        RandomListNode copyTemp =pCopy;
+        while(copyTemp.next != null){
+            pHeadtemp.next = copyTemp.next;
+            pHeadtemp = pHeadtemp.next;
+            copyTemp.next = pHeadtemp.next;
+            copyTemp =copyTemp.next;
+        }
+        return pCopy;
+    }
+
+}
+
+ class RandomListNode {
+    int label;
+    RandomListNode next = null;
+    RandomListNode random = null;
+
+    RandomListNode(int label) {
+        this.label = label;
+    }
+}
